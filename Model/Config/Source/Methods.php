7f419115aeca39cb7c3d9c0956ac7a5e5a561f26
Vinod Sowdagar <53818758+Vinod-MultiSafepay@users.noreply.github.com>
2020-12-09 10:58:30 +0100

    Feature/default preselected method (#70)
    * Add config for default preselect method
    
    * Add to CHANGELOG.md

---
+++ Model/Config/Source/Methods.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is provided with Magento in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * Copyright © 2020 MultiSafepay, Inc. All rights reserved.
+ * See DISCLAIMER.md for disclaimer details.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace MultiSafepay\ConnectAdminhtml\Model\Config\Source;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Data\OptionSourceInterface;
+
+class Methods implements OptionSourceInterface
+{
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * Methods constructor.
+     *
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function toOptionArray(): array
+    {
+        return $this->getAllMethods();
+    }
+
+    /**
+     * @return array
+     */
+    public function getAllMethods(): array
+    {
+        $methodList = $this->scopeConfig->getValue('payment');
+
+        $methods = [];
+
+        foreach ($methodList as $code => $method) {
+            if (isset($method['is_multisafepay'])) {
+                $methods[] = [
+                    'value' => $code,
+                    'label' => $method['title']
+                ];
+            }
+        }
+
+        return $methods;
+    }
+}
