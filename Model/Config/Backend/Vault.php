8b1b6920b45e3e6fc1414b2e7b3c80bf0f79ea68
Vinod Sowdagar <53818758+Vinod-MultiSafepay@users.noreply.github.com>
2021-02-19 18:11:45 +0100

    Feature/creditcard vault (#72)
    * Add vault enabling field to the backend config
    
    * Add Vault for Visa gateway
    
    * Add Vault for Mastercard gateway
    
    * Add Vault for Amex gateway
    
    * Add general config for enabling/disabling supported gateways
    
    * Fix text in comments
    
    * Add feedback changes

---
+++ Model/Config/Backend/Vault.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is provided with Magento in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * Copyright © 2021 MultiSafepay, Inc. All rights reserved.
+ * See DISCLAIMER.md for disclaimer details.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace MultiSafepay\ConnectAdminhtml\Model\Config\Backend;
+
+use Magento\Framework\App\Cache\TypeListInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\Config\Storage\WriterInterface;
+use Magento\Framework\App\Config\Value;
+use Magento\Framework\Data\Collection\AbstractDb;
+use Magento\Framework\Model\Context;
+use Magento\Framework\Model\ResourceModel\AbstractResource;
+use Magento\Framework\Registry;
+use MultiSafepay\ConnectCore\Model\Vault as VaultModel;
+
+class Vault extends Value
+{
+    /**
+     * @var WriterInterface
+     */
+    private $writer;
+
+    /**
+     * Vault constructor.
+     *
+     * @param Context $context
+     * @param Registry $registry
+     * @param ScopeConfigInterface $config
+     * @param TypeListInterface $cacheTypeList
+     * @param WriterInterface $writer
+     * @param AbstractResource|null $resource
+     * @param AbstractDb|null $resourceCollection
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        Registry $registry,
+        ScopeConfigInterface $config,
+        TypeListInterface $cacheTypeList,
+        WriterInterface $writer,
+        AbstractResource $resource = null,
+        AbstractDb $resourceCollection = null,
+        array $data = []
+    ) {
+        parent::__construct($context, $registry, $config, $cacheTypeList, $resource, $resourceCollection, $data);
+        $this->writer = $writer;
+    }
+
+    /**
+     * @return Vault
+     */
+    public function afterSave(): Vault
+    {
+        foreach (array_keys(VaultModel::VAULT_GATEWAYS) as $method) {
+            $path = 'payment/' . $method . '_vault/active';
+
+            if ($this->_config->getValue($path) !== $this->getValue()) {
+                $this->writer->save($path, $this->getValue(), $this->getScope(), $this->getScopeId());
+            }
+        }
+
+        return parent::afterSave();
+    }
+}
