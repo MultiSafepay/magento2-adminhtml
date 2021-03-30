3fa93960b335719093680e191a60cf88cc6a10bc
Vinod MultiSafepay <vinod.sowdagar@multisafepay.com>
2021-03-30 16:11:44 +0200

    Fix file names and zip file/directory paths

--- Controller/Adminhtml/Download/MultiSafepayLog.php
+++ Controller/Adminhtml/Download/MultiSafepayLog.php
@@ -10,7 +10,9 @@ use Magento\Backend\App\Action\Context;
 use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\App\Response\Http\FileFactory;
 use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Archive\Zip;
 use Magento\Framework\Exception\FileSystemException;
+use Magento\Framework\Filesystem\Driver\File;
 use MultiSafepay\ConnectCore\Logger\Logger;
 use ZipArchive;
 
@@ -25,6 +27,14 @@ class MultiSafepayLog extends Action
      * @var DirectoryList
      */
     protected $directoryList;
+    /**
+     * @var File
+     */
+    protected $driverFile;
+    /**
+     * @var Zip
+     */
+    protected $zip;
 
     /**
      * @var FileFactory
@@ -36,28 +46,34 @@ class MultiSafepayLog extends Action
      *
      * @param DirectoryList $directoryList
      * @param FileFactory $fileFactory
+     * @param File $driverFile
      * @param Logger $logger
+     * @param Zip $zip
      * @param Context $context
      */
     public function __construct(
         DirectoryList $directoryList,
         FileFactory $fileFactory,
+        File $driverFile,
         Logger $logger,
+        Zip $zip,
         Context $context
     ) {
         parent::__construct($context);
         $this->fileFactory = $fileFactory;
         $this->logger = $logger;
         $this->directoryList = $directoryList;
+        $this->driverFile = $driverFile;
+        $this->zip = $zip;
     }
 
     /**
      * @return ResponseInterface
      * @throws Exception
      */
-    public function execute()
+    public function execute(): ResponseInterface
     {
-        //do your custom stuff here
+        /*
         $fileName = 'multisafepay.log';
         return $this->fileFactory->create(
             $fileName,
@@ -67,40 +83,52 @@ class MultiSafepayLog extends Action
             ],
             DirectoryList::LOG
         );
+        */
+
+        /*
+        $directory = $this->directoryList->getPath(DirectoryList::LOG) . '/multisafepay.log';
+        $this->zip->pack($directory, 'test.zip');
+        */
+
+        return $this->zipLogFiles();
     }
 
-    public function zipLogFiles()
+    /**
+     * @throws FileSystemException
+     * @throws Exception
+     */
+    public function zipLogFiles(): ResponseInterface
     {
         if (!class_exists(ZipArchive::class)) {
             $this->logger->error('\ZipArchive class not found, zip file could not be created.');
         }
-        try {
-            $directory = $this->directoryList->getPath(DirectoryList::LOG);
 
-            $zipFile = new ZipArchive();
-            $zipFile->open('MultiSafepayLogs.zip', ZipArchive::CREATE | ZipArchive::OVERWRITE);
+        $directory = $this->directoryList->getPath(DirectoryList::LOG);
 
-            $files = new \RecursiveIteratorIterator(
-                new \RecursiveDirectoryIterator($directory),
-                \RecursiveIteratorIterator::LEAVES_ONLY
-            );
+        $zipFile = new ZipArchive();
+        $zipFile->open(
+            'MultiSafepayLogs.zip',
+            ZipArchive::CREATE | ZipArchive::OVERWRITE
+        );
 
-            foreach ($files as $name => $file) {
-                // Skip directories (they would be added automatically)
-                if (!$file->isDir()) {
-                    // Get real and relative path for current file
-                    $filePath = $file->getRealPath();
-                    $relativePath = substr($filePath, strlen($directory) + 1);
+        $files = $this->driverFile->readDirectory($directory);
 
-                    // Add current file to archive
-                    $zipFile->addFile($filePath, $relativePath);
-                }
+        foreach ($files as $filePath) {
+            if (strpos($filePath, 'multisafepay') !== false) {
+                $zipFile->addFile($filePath, basename($filePath));
             }
-            $zipFile->close();
-
-            return $zipFile;
-        } catch (FileSystemException $e) {
-            $this->logger->logFileSystemException($e);
         }
+        $zipFile->close();
+
+        return $this->fileFactory->create(
+            'MultiSafepayLogs.zip',
+            [
+                'type' => 'filename',
+                'value' => 'MultiSafepayLogs.zip',
+                'rm' => true
+            ],
+            DirectoryList::ROOT,
+            'application/zip'
+        );
     }
 }
