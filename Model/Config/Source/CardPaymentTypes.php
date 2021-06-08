a85d94993cde0b2ef4d4af28dfa887a82419a0b9
Stanislau Kharlap <stanislau.kharlap@multisafepay.com>
2021-06-08 13:39:15 +0200

    finished credit card component feature

--- Model/Config/Source/CardPaymentTypes.php
+++ Model/Config/Source/CardPaymentTypes.php
@@ -22,6 +22,7 @@ use Magento\Framework\Data\OptionSourceInterface;
 class CardPaymentTypes implements OptionSourceInterface
 {
     public const REDIRECT_PAYMENT_TYPE = 'redirect';
+    public const CREDIT_CARD_COMPONENT_PAYMENT_TYPE = 'credit_card';
     public const PAYMENT_REQUEST_PAYMENT_TYPE = 'payment_request';
 
     /**
@@ -31,6 +32,7 @@ class CardPaymentTypes implements OptionSourceInterface
     {
         return [
             ['value' => self::REDIRECT_PAYMENT_TYPE, 'label' => __('Redirect')],
+            ['value' => self::CREDIT_CARD_COMPONENT_PAYMENT_TYPE, 'label' => __('Credit Card Component')],
             ['value' => self::PAYMENT_REQUEST_PAYMENT_TYPE, 'label' => __('Payment Request API')]
         ];
     }
