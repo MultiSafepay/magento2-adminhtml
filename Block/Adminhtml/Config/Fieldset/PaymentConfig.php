2d81149c7463ec92b5e0dfea44ba98f67af62604
Vinod MultiSafepay <vinod.sowdagar@multisafepay.com>
2021-07-28 16:12:50 +0200

    Add button handling

---
+++ Block/Adminhtml/Config/Fieldset/PaymentConfig.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is provided with Magento in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * Copyright © 2021 MultiSafepay, Inc. All rights reserved.
+ * See DISCLAIMER.md for disclaimer details.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace MultiSafepay\ConnectAdminhtml\Block\Adminhtml\Config\Fieldset;
+
+use Magento\Backend\Block\Context;
+use Magento\Backend\Model\Auth\Session;
+use Magento\Config\Block\System\Config\Form\Fieldset;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+use Magento\Framework\View\Helper\Js;
+
+class PaymentConfig extends Fieldset
+{
+    /**
+     * @param Context $context
+     * @param Session $authSession
+     * @param Js $jsHelper
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        Session $authSession,
+        Js $jsHelper,
+        array $data = []
+    ) {
+        parent::__construct($context, $authSession, $jsHelper, $data);
+    }
+
+    /**
+     * Add custom css class
+     *
+     * @param AbstractElement $element
+     * @return string
+     */
+    protected function _getFrontendClass($element): string
+    {
+        return parent::_getFrontendClass($element) . ' with-button';
+    }
+
+    /**
+     * Return header title part of html for payment solution
+     *
+     * @param AbstractElement $element
+     * @return string
+     * @SuppressWarnings(PHPMD.NPathComplexity)
+     */
+    protected function _getHeaderTitleHtml($element): string
+    {
+        $html = '<div class="config-heading" >';
+        $htmlId = $element->getHtmlId();
+        $html .= '<div class="button-container"><button type="button"' .
+                 ' class="button action-configure' . '" id="' . $htmlId . '-head" 
+                 onclick="' . "location.href='" . $this->escapeUrl(
+                     $this->getUrl('adminhtml/system_config/edit/section/multisafepay_support')
+                ) . "'" . '"><span 
+                 class="state-closed">' . __(
+                     'Configure'
+                 ) . '</span></button>';
+
+        $html .= '</div>';
+        $html .= '<div class="heading"><strong>' . $element->getLegend() . '</strong>';
+
+        if ($element->getComment()) {
+            $html .= '<span class="heading-intro">' . $element->getComment() . '</span>';
+        }
+        $html .= '</div></div>';
+
+        return $html;
+    }
+
+    /**
+     * Return header comment part of html for payment solution
+     *
+     * @param AbstractElement $element
+     * @return string
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    protected function _getHeaderCommentHtml($element): string
+    {
+        return '';
+    }
+
+    /**
+     * Get collapsed state on-load
+     *
+     * @param AbstractElement $element
+     * @return false
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    protected function _isCollapseState($element): bool
+    {
+        return false;
+    }
+}
