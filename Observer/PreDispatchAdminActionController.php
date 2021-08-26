87f315c04d3fab21e661655d38f0a427831956a5
Stanislau Kharlap <stanislau.kharlap@multisafepay.com>
2021-08-26 15:11:31 +0200

    changed event listeners

--- Observer/PreDispatchAdminActionController.php
+++ Observer/PreDispatchAdminActionController.php
@@ -16,7 +16,6 @@
 namespace MultiSafepay\ConnectAdminhtml\Observer;
 
 use Exception;
-use Magento\Backend\Model\Auth\Session;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use MultiSafepay\ConnectCore\Logger\Logger;
@@ -30,11 +29,6 @@ class PreDispatchAdminActionController implements ObserverInterface
     private $notificationUtil;
 
     /**
-     * @var Session
-     */
-    private $backendSession;
-
-    /**
      * @var Logger
      */
     private $logger;
@@ -43,16 +37,13 @@ class PreDispatchAdminActionController implements ObserverInterface
      * PreDispatchAdminActionController constructor.
      *
      * @param NotificationUtil $notificationUtil
-     * @param Session $backendAuthSession
      * @param Logger $logger
      */
     public function __construct(
         NotificationUtil $notificationUtil,
-        Session $backendAuthSession,
         Logger $logger
     ) {
         $this->notificationUtil = $notificationUtil;
-        $this->backendSession = $backendAuthSession;
         $this->logger = $logger;
     }
 
@@ -63,12 +54,10 @@ class PreDispatchAdminActionController implements ObserverInterface
      */
     public function execute(Observer $observer)
     {
-        if ($this->backendSession->isLoggedIn()) {
-            try {
-                $this->notificationUtil->addNewReleaseNotification();
-            } catch (Exception $exception) {
-                $this->logger->critical($exception);
-            }
+        try {
+            $this->notificationUtil->addNewReleaseNotification();
+        } catch (Exception $exception) {
+            $this->logger->critical($exception);
         }
     }
 }
