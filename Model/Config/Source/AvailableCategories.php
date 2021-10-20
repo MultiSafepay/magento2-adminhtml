3ba4230c5a591530aa2e72db02d2d84d1e4a6e78
Stanislau Kharlap <stanislau.kharlap@multisafepay.com>
2021-10-20 16:54:45 +0200

    added available categories recursive tree

---
+++ Model/Config/Source/AvailableCategories.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is provided with Magento in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * Copyright © 2021 MultiSafepay, Inc. All rights reserved.
+ * See DISCLAIMER.md for disclaimer details.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace MultiSafepay\ConnectAdminhtml\Model\Config\Source;
+
+use Magento\Catalog\Model\ResourceModel\Category\CollectionFactory;
+use Magento\Framework\Data\OptionSourceInterface;
+use Magento\Framework\Exception\LocalizedException;
+
+class AvailableCategories implements OptionSourceInterface
+{
+    public const ALL_CATEGORIES = 'all_categories';
+    public const SYSTEM_CATEGORY_ID = 1;
+    public const ROOT_LEVEL = 1;
+
+    /**
+     * @var CollectionFactory
+     */
+    private $collectionFactory;
+
+    /**
+     * @var array
+     */
+    private $categories = [];
+
+    /**
+     * AvailableCategories constructor.
+     *
+     * @param CollectionFactory $collectionFactory
+     */
+    public function __construct(
+        CollectionFactory $collectionFactory
+    ) {
+        $this->collectionFactory = $collectionFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function toOptionArray(): array
+    {
+        if (!$this->categories) {
+            $this->categories = $this->getChildren(self::SYSTEM_CATEGORY_ID, self::ROOT_LEVEL);
+        }
+
+        return $this->categories;
+    }
+
+    /**
+     * @param int $parentCategoryId
+     * @param int $level
+     * @return array
+     * @throws LocalizedException
+     */
+    private function getChildren(int $parentCategoryId, int $level): array
+    {
+        $options = [
+            ['value' => self::ALL_CATEGORIES, 'label' => __('All Categories')],
+        ];
+        $collection = $this->collectionFactory->create();
+        $collection->addAttributeToSelect('name');
+        $collection->addAttributeToFilter('level', $level);
+        $collection->addAttributeToFilter('parent_id', $parentCategoryId);
+        $collection->addAttributeToFilter('is_active', 1);
+        $collection->setOrder('position', 'asc');
+
+        foreach ($collection as $category) {
+            if ($category->getLevel() > self::ROOT_LEVEL) {
+                $options[$category->getId()] = [
+                    'value' => $category->getId(),
+                    'label' => str_repeat(". ", max(0, ($category->getLevel() - 1) * 3))
+                               . $category->getName(),
+                ];
+            }
+
+            if ($category->hasChildren()) {
+                $options = array_replace(
+                    $options,
+                    $this->getChildren(
+                        (int)$category->getId(),
+                        $category->getLevel() + 1
+                    )
+                );
+            }
+        }
+
+        return $options;
+    }
+}
