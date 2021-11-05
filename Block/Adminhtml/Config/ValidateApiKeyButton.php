62283b6f17a2d4cf4fde540b102a6a2f838608a2
Stanislau Kharlap <stanislau.kharlap@multisafepay.com>
2021-11-05 17:45:46 +0100

    added button to verify api keys

---
+++ Block/Adminhtml/Config/ValidateApiKeyButton.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is provided with Magento in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * Copyright © 2021 MultiSafepay, Inc. All rights reserved.
+ * See DISCLAIMER.md for disclaimer details.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace MultiSafepay\ConnectAdminhtml\Block\Adminhtml\Config;
+
+use Magento\Config\Block\System\Config\Form\Field;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Backend\Block\Widget\Button;
+use Magento\Backend\Block\Template;
+
+class ValidateApiKeyButton extends Field
+{
+    private const TEMPLATE_PATH = 'MultiSafepay_ConnectAdminhtml::config/general/validate_api_key_button.phtml';
+    private const CHECK_BUTTON_ID = 'multisafepay_validate_button';
+
+    /**
+     * @param AbstractElement $element
+     * @return string
+     * @throws LocalizedException
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    protected function _getElementHtml(AbstractElement $element): string
+    {
+        /** @var Template $block */
+        $block = $this->_layout->createBlock(Template::class);
+        $button = $this->getLayout()->createBlock(Button::class)
+            ->setData([
+                'id' => self::CHECK_BUTTON_ID,
+                'label' => __('Check API Key'),
+                'class' => 'primary',
+            ]);
+        $block->setTemplate(self::TEMPLATE_PATH)
+            ->setData('send_button', $button->toHtml())
+            ->setData('ajax_url', $this->getAjaxUrl())
+            ->setData('button_id', self::CHECK_BUTTON_ID);
+
+        return $block->toHtml();
+    }
+
+    /**
+     * @param AbstractElement $element
+     * @return string
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    protected function _renderScopeLabel(AbstractElement $element): string
+    {
+        return '';
+    }
+
+    /**
+     * @return string
+     */
+    private function getAjaxUrl(): string
+    {
+        return $this->getUrl('multisafepay/api/validate', ['_secure' => true]);
+    }
+
+    /**
+     * @return string
+     */
+    public function getCheckButtonId(): string
+    {
+        return self::CHECK_BUTTON_ID;
+    }
+}
---
+++ Controller/Adminhtml/Api/Validate.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is provided with Magento in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * Copyright © 2021 MultiSafepay, Inc. All rights reserved.
+ * See DISCLAIMER.md for disclaimer details.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace MultiSafepay\ConnectAdminhtml\Controller\Adminhtml\Api;
+
+use Exception;
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\ResponseInterface;
+use MultiSafepay\ConnectCore\Factory\SdkFactory;
+use MultiSafepay\ConnectCore\Logger\Logger;
+use MultiSafepay\ConnectCore\Util\JsonHandler;
+use Psr\Http\Client\ClientExceptionInterface;
+
+class Validate extends Action
+{
+    private const MODE_PARAM_KEY_NAME = 'mode';
+    private const API_KEY_PARAM_KEY_NAME = 'apiKey';
+
+    /**
+     * @var JsonHandler
+     */
+    private $jsonHandler;
+
+    /**
+     * @var SdkFactory
+     */
+    private $sdkFactory;
+
+    /**
+     * @var Logger
+     */
+    private $logger;
+
+    /**
+     * Validate constructor.
+     *
+     * @param Context $context
+     * @param JsonHandler $jsonHandler
+     * @param SdkFactory $sdkFactory
+     * @param Logger $logger
+     */
+    public function __construct(
+        Context $context,
+        JsonHandler $jsonHandler,
+        SdkFactory $sdkFactory,
+        Logger $logger
+    ) {
+        parent::__construct($context);
+        $this->jsonHandler = $jsonHandler;
+        $this->sdkFactory = $sdkFactory;
+        $this->logger = $logger;
+    }
+
+    /**
+     * @return ResponseInterface
+     */
+    public function execute(): ResponseInterface
+    {
+        try {
+            if (($data = $this->getRequest()->getParams())
+                && isset($data[self::MODE_PARAM_KEY_NAME], $data[self::API_KEY_PARAM_KEY_NAME])
+            ) {
+                $this->sdkFactory->createWithModeAndApiKey(
+                    (bool)$data[self::MODE_PARAM_KEY_NAME],
+                    (string)$data[self::API_KEY_PARAM_KEY_NAME],
+                )->getAccountManager()->get()->getData();
+
+                $result = [
+                    'status' => true,
+                    'content' => __('API key is valid.'),
+                ];
+            } else {
+                $result = [
+                    'status' => false,
+                    'content' => __('Error. Something went wrong. Please, try again.'),
+                ];
+            }
+        } catch (ClientExceptionInterface $clientException) {
+            $result = [
+                'status' => false,
+                'content' => $clientException->getMessage(),
+            ];
+        } catch (Exception $exception) {
+            $result = [
+                'status' => false,
+                'content' => $exception->getMessage(),
+            ];
+        }
+
+        return $this->getResponse()->representJson(
+            $this->jsonHandler->convertToJSON($result)
+        );
+    }
+}
