fa527a505387705f1206cf25c489099c2d48045b
Stanislau Kharlap <78361324+Stanislau-MultiSafepay@users.noreply.github.com>
2022-01-10 14:13:10 +0100

    added draft for setting (#142)
    * added draft for setting
    
    * added fix
    
    * fixed comments after cr
    
    * moved constants to different place

---
+++ Model/Config/Source/SecondChancePaymentLink.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is provided with Magento in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * Copyright © 2021 MultiSafepay, Inc. All rights reserved.
+ * See DISCLAIMER.md for disclaimer details.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace MultiSafepay\ConnectAdminhtml\Model\Config\Source;
+
+use Magento\Framework\Data\OptionSourceInterface;
+use MultiSafepay\ConnectCore\Service\Order\CancelMultisafepayOrderPaymentLink;
+
+class SecondChancePaymentLink implements OptionSourceInterface
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function toOptionArray(): array
+    {
+        return [
+            [
+                'value' => CancelMultisafepayOrderPaymentLink::DISABLE_PRETRANSACTION_CANCELLING_OPTION,
+                'label' => __('Don\'t cancel')
+            ],
+            [
+                'value' => CancelMultisafepayOrderPaymentLink::CANCEL_BACK_BUTTON_PRETRANSACTION_OPTION,
+                'label' => __('Cancel when cancel button is pressed')
+            ],
+            [
+                'value' => CancelMultisafepayOrderPaymentLink::CANCEL_ALWAYS_PRETRANSACTION_OPTION,
+                'label' => __('Cancel when cancel button pressed and when order is canceled')
+            ]
+        ];
+    }
+}
