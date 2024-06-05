61381950dc43c5c54660ccf8ea4a8c4cf49cbadc
Vinod Sowdagar <53818758+vinodsowdagar@users.noreply.github.com>
2024-06-05 10:18:00 +0200

    PLGMAG2V2-760: Add to order payment description with which type of card was paid for Card Payment (#234)
    * PLGMAG2V2-760: PLGMAG2V2-760: Add to order payment description with which type of card was paid for Card Payment
    
    * Add CR feedback
    
    * Add underscore to make it more robust

--- Plugin/Payment/Block/InfoPlugin.php
+++ Plugin/Payment/Block/InfoPlugin.php
@@ -17,6 +17,9 @@ use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\LayoutInterface;
 use Magento\Payment\Block\Info;
+use Magento\Payment\Helper\Data;
+use MultiSafepay\ConnectCore\Model\Ui\Gateway\CreditCardConfigProvider;
+use MultiSafepay\ConnectCore\Service\Process\AddCardPaymentInformation;
 use MultiSafepay\ConnectCore\Util\AmountUtil;
 use MultiSafepay\ConnectCore\Util\GiftcardUtil;
 use MultiSafepay\ConnectCore\Util\PaymentMethodUtil;
@@ -47,23 +50,31 @@ class InfoPlugin
     private $escaper;
 
     /**
+     * @var Data
+     */
+    private $paymentHelper;
+
+    /**
      * InfoPlugin constructor.
      *
      * @param LayoutInterface $layout
      * @param PaymentMethodUtil $paymentMethodUtil
      * @param AmountUtil $amountUtil
      * @param Escaper $escaper
+     * @param Data $paymentHelper
      */
     public function __construct(
         LayoutInterface $layout,
         PaymentMethodUtil $paymentMethodUtil,
         AmountUtil $amountUtil,
-        Escaper $escaper
+        Escaper $escaper,
+        Data $paymentHelper
     ) {
         $this->layout = $layout;
         $this->paymentMethodUtil = $paymentMethodUtil;
         $this->amountUtil = $amountUtil;
         $this->escaper = $escaper;
+        $this->paymentHelper = $paymentHelper;
     }
 
     /**
@@ -101,5 +112,47 @@ class InfoPlugin
         }
 
         return $subject;
+    }
+
+    /**
+     * Whenever a card payment has been made, append the card scheme to know with which type of card has been paid
+     *
+     * @param Info $subject
+     * @param $result
+     * @return mixed
+     * @throws LocalizedException
+     */
+    public function afterToHtml(Info $subject, $result)
+    {
+        $methodCode = $subject->getMethod()->getCode();
+        $isMultiSafepayPaymentMethod = $this->paymentMethodUtil->isMultisafepayPaymentByCode($methodCode);
+        $isCardPayment = $methodCode === CreditCardConfigProvider::CODE;
+
+        if (!$isMultiSafepayPaymentMethod && !$isCardPayment) {
+            return $result;
+        }
+
+        $additionalInformation = $subject->getInfo()->getAdditionalInformation();
+
+        if (!isset($additionalInformation[AddCardPaymentInformation::CARD_PAYMENT_ADDITIONAL_DATA_PARAM_NAME])) {
+            return $result;
+        }
+
+        $cardCode = $additionalInformation[AddCardPaymentInformation::CARD_PAYMENT_ADDITIONAL_DATA_PARAM_NAME];
+        $methods = $this->paymentHelper->getPaymentMethods();
+
+        foreach ($methods as $code => $method) {
+            if (strpos($code, '_recurring') !== false) {
+                continue;
+            }
+
+            if (isset($method['gateway_code']) && $method['gateway_code'] === $cardCode) {
+                $methodTitle = $method['title'];
+
+                return $result . __(' - Paid with ') . $methodTitle;
+            }
+        }
+
+        return $result;
     }
 }
